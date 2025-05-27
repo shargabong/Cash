98927e581b19c5c3fb40a855f31933e877cea263
shargabong <shargabong.exe@gmail.com>
2025-05-27 10:08:23 +0300

    Update Program.cs

--- Cash/Program.cs
+++ Cash/Program.cs
@@ -166,7 +166,7 @@ namespace BankingAppCSharpSimple
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.InputEncoding = System.Text.Encoding.UTF8;
-            Console.WriteLine("Добро пожаловать в Упрощенное Банковское Приложение!");
+            Console.WriteLine("Добро пожаловать!");
 
             try
             {
